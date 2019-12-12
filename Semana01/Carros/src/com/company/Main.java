fccf2f7fc2e49b655d36b66f2c531186353ef6bd
MarcioTomaz <marciotmaz@gmail.com>
2019-12-11 23:27:14 -0300

    Aula Classes e objetos exemplo

--- Semana01/Carros/src/com/company/Main.java
+++ Semana01/Carros/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            Carro c1 = new Carro();
+            c1.potencia = 10;
     }
 }
